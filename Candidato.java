25cae39318c530ff7016e63c0be29af0c5ecf086
oeduardosg <eduardosg.my@gmail.com>
2025-02-20 08:18:46 -0300

    Adiciona e melhora funções de idade

--- Candidato.java
+++ Candidato.java
@@ -3,19 +3,19 @@ public class Candidato {
     private int numero;
     private String nome;
     private Partido partido;
-    private int idade;
-    private boolean eleito;
+    private String nascimento;
+    private int eleito;
     private int genero;
     private int votos;
 
-    public Candidato(int numero, String nome, Partido partido, int idade, int genero, boolean eleito) {
+    public Candidato(int numero, String nome, Partido partido, String nascimento, int genero, int eleito) {
         this.numero = numero;
         this.nome = nome;
         this.partido = partido;
-        this.idade = idade;
+        this.nascimento = nascimento;
         this.genero = genero;
         this.eleito = eleito;
-        if(eleito) partido.aumentaQuantidadeEleitos();
+        if(eleito > 0) partido.aumentaQuantidadeEleitos();
     }
 
     public int getNumero() {
@@ -33,13 +33,13 @@ public class Candidato {
     }
 
 
-    public int getIdade() {
-        return idade;
+    public String getNascimento() {
+        return nascimento;
     }
 
 
     public boolean isEleito() {
-        return eleito;
+        return eleito > 0;
     }
 
 
@@ -52,13 +52,40 @@ public class Candidato {
         return votos;
     }
 
+    public int getIdade() {
+        String[] datas = getNascimento().split("/");
+
+        int idade = 2024 - Integer.valueOf(datas[2]);
+
+        if(Integer.valueOf(datas[1]) < 10) return idade;
+        else if(Integer.valueOf(datas[1]) > 10) return idade - 1;
+        else {
+            if(Integer.valueOf(datas[0]) <= 28) return idade;
+            else return idade - 1;
+        }
+
+    }
+
+    public int getMaisVelho(Candidato x, Candidato y) {
+        String[] nascimentoX = x.getNascimento().split("/");
+        String[] nascimentoY = x.getNascimento().split("/");
+
+        if(Integer.valueOf(nascimentoX[2]) < Integer.valueOf(nascimentoY[2])) return 1;
+        else if(Integer.valueOf(nascimentoX[2]) > Integer.valueOf(nascimentoY[2])) return -1;
+        else {
+            if(Integer.valueOf(nascimentoX[1]) < Integer.valueOf(nascimentoY[1])) return 1;
+            else if(Integer.valueOf(nascimentoX[1]) > Integer.valueOf(nascimentoY[1])) return -1;
+            else {
+                if(Integer.valueOf(nascimentoX[0]) < Integer.valueOf(nascimentoY[0])) return 1;
+                else if(Integer.valueOf(nascimentoX[0]) > Integer.valueOf(nascimentoY[0])) return -1;
+                else return 0;
+            }           
+        }
+    }
+
     public void aumentaVotos(int novosVotos) {
         partido.adicionaVotos(novosVotos);
         votos += novosVotos;
-    }
-
-    public void eleito() {
-        this.eleito = true;
     }
 
     @Override
